7f4183a5ec0f250674be3c6031ec26ec5d6a5bf2
paulaa99 <paau.a@hotmail.com>
2023-08-07 20:02:39 -0300

    arreglos en login

---
+++ js/login.js
@@ -0,0 +1,7 @@
+const formulario = document.getElementById('loginForm');
+
+loginForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    window.location.href = 'index.html';
+});
